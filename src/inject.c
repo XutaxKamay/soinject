31c2e77941abd5be6819f047c7c9ef4e157411ad
XutaxKamay <xutaxkamay@gmail.com>
2019-05-19 00:07:34 +0200

    Fixed comment again

--- src/inject.c
+++ src/inject.c
@@ -503,7 +503,7 @@ ptr_t remote_mmap(pid_t pid,
     ptr_u_t data, out, remote_mmap_addr;
     int status;
 
-    // Let's find mmap first!
+    // Let's find munmap first!
     remote_mmap_addr = get_remote_sym("libc", "mmap", pid);
 
     if (remote_mmap_addr.p == NULL)
